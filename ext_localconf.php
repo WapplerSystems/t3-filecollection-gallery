581bd2890852ed6fcbff111ba76e013bc29f851f
Sven Wappler <info@wappler.systems>
2023-09-28 00:58:47 +0200

    update

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,13 @@
 defined('TYPO3') or die();
 
 use \TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use WapplerSystems\FilecollectionGallery\Controller\GalleryController;
 
 ExtensionUtility::configurePlugin(
     'filecollection_gallery',
     'Gallery',
     [
-        \WapplerSystems\FilecollectionGallery\Controller\GalleryController::class => 'list,nested,nestedFromFolder,listFromFolder',
+        GalleryController::class => 'list,nested,nestedFromFolder,listFromFolder',
     ],
     [],
     ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
